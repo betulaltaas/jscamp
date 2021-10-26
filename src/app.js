39663cb561571a8324be0097bac0bff5d7c0d726
betulaltaas <betul.altas34@gmail.com>
2021-10-26 15:22:03 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,24 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+{
+    let dolarDun = 9.10
+}
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11  
+console.log(euroDun)
+//array
+//camelCasing
+//PascalCasing 
+let konutKredileri= ["Konut Kredisi", "Emlak Konut Kredisi" , "Kamu Konut Kredisi"]
+console.log("<ul>")
+
+for (let index = 0; index < konutKredileri.length; index++) {
+   
+    console.log("<li>" + konutKredileri[index] + "</li>")
+    
+}
+console.log("</ul>")
